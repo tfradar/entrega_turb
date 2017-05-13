b5e7e9c6cd4f2cc2d7898fa9bbfd739a7dc44dcb
Toni Frau Darder <ToniFrau@MacBook-Pro-4.local>
2017-05-13 12:41:33 +0200

    Added turbo-propeller function and propeller performance function

--- entrega_turb.py
+++ entrega_turb.py
@@ -73,11 +73,27 @@ def relacion_compresor_normal(G0):
         return 0
     return rel_c
 
+##Función rendimiento de la hélice
+def rendimiento_helice(mach_i, alt_i):
+    altitud = []
+    rendimientos = np.zeros([12, 16])
+    mach = [0, 0.1, 0.2, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9]
+    for ii, alt in enumerate(np.linspace(0, 11000, 12)):
+        x = np.linspace(0, 1, 16)
+        altitud.append(alt)
+        rendimientos[ii] = (-(x - 0.2) ** 2) * (10 - ii) / 10
+        rendimientos[ii] = rendimientos[ii] + .95 ** 2 - 0.975 * ii / 11
+        rendimientos[ii] = rendimientos[ii] * ((ii + 12) / 11)
+    rendimientos[11] = rendimientos[11] * 0
+    altitud = np.asarray(altitud)
+    indice = np.where(altitud <= alt_i)
+    return np.interp(mach_i, mach, rendimientos[indice[-1][-1]])
+
+##Función generador de gas
 def generador_gas(P0, T0, G0):
     # Compresor:
     P3t = P0 * relacion_compresor_normal(G0)
     T3t = T0 * (1 + 1 / rend_comb * (-1 + pi_comb ** ((air.gamma_air(T2t) - 1) / air.gamma_air(T2t))))
-
     # Cámara de combustión
     T4t = f * L * rend_comb / (1 + f) / air.cp_air(T3t) + T3t / (1 + f)
     c = f * G0
@@ -91,7 +107,7 @@ def generador_gas(P0, T0, G0):
     W23 = G0 * air.cp_air(Tcomp) * (T3t - T2t)
     P5t = ((T5t / T4t - 1) / rend_turb(T4t) + 1) ** (air.gamma_air(Tturb) / (air.gamma_air(Tturb) - 1)) * P4t
 
-
+##Función Turborreactor
 def turborreactor(m, T0, G0, p0):
     # Difusor:
     T2t = gas.stagnation_temperature_from_mach(m, T0)
@@ -100,7 +116,6 @@ def turborreactor(m, T0, G0, p0):
     # Compresor:
     P3t = P2t * relacion_compresor_normal(G0)
     T3t = T2t * (1 + 1 / rend_comb * (-1 + pi_comb ** ((air.gamma_air(T2t) - 1) / air.gamma_air(T2t))))
-
     # Cámara de combustión
     T4t = f * L * rend_comb / (1 + f) / air.cp_air(T3t) + T3t / (1 + f)
     c = f * G0
@@ -119,7 +134,38 @@ def turborreactor(m, T0, G0, p0):
     T9 = T5t * (
     rend_tobera(h) * ((P9 / P5t) ** (air.gamma_air((T5t + T2t) / 2) - 1) / air.gamma_air((T5t + T2t) / 2) - 1) + 1)
 
+##Función Turbohélice
+def turbohelice(m, G0, T0, p0):
+    # Difusor:
+    T2t = gas.stagnation_temperature_from_mach(m, T0)
+    P2t = gas.stagnation_pressure_from_mach(m, p0, T0)
 
+    # Compresor:
+    P3t = P2t * relacion_compresor_normal(G0)
+    T3t = T2t * (1 + 1 / rend_comb * (-1 + pi_comb ** ((air.gamma_air(T2t) - 1) / air.gamma_air(T2t))))
+
+    # Cámara de combustión
+    T4t = f * L * rend_comb / (1 + f) / air.cp_air(T3t) + T3t / (1 + f)
+    c = f * G0
+    Tcomp = 0.5 * (T3t + T2t)
+    P4t = pi_comb * P3t
+
+    # Turbina 1
+    T5t = T4t - air.cp_air(Tcomp) / air.cp_air((T3t + T4t) / 2) * (T3t - T2t)
+    Tturb = 0.5 * (T5t + T4t)
+    W45 = G0 * air.cp_air(Tturb) * (T5t - T4t)
+    W23 = G0 * air.cp_air(Tcomp) * (T3t - T2t)
+    P5t = ((T5t / T4t - 1) / rend_turb(T4t) + 1) ** (air.gamma_air(Tturb) / (air.gamma_air(Tturb) - 1)) * P4t
+    
+    # Turbina 2
+    T6t = T5t - air.cp_air(Tcomp) / air.cp_air((T4t + T5t) / 2) * (T4t - T3t)
+    Tturb2 = 0.5 * (T6t + T5t)
+    W45 = G0 * air.cp_air(Tturb2) * (T6t - T5t)
+    W23 = G0 * air.cp_air(Tcomp) * (T4t - T3t)
+    P6t = ((T6t / T5t - 1) / 0.95 + 1) ** (air.gamma_air(Tturb2) / (air.gamma_air(Tturb2) - 1)) * P5t
+
+    
+    
 height = np.array([500, 2000, 4000, 5000, 7000, 9000, 11000])
 mach = np.array([0.3, .45, .6, .8, .85, .9, .95])
 
@@ -138,6 +184,4 @@ for h in height:
         v0 = m * gas.sound_speed(T0[ii])
         G0 = rho0 * area * v0
 
-        turborreactor(m, G0,T0[ii], p0[ii])
-        
         ii += 1
