ea7304366e5ea3e78d370dacbce6cc0651e8acf8
Toni Frau Darder <ToniFrau@MacBook-Pro-4.local>
2017-05-15 12:17:48 +0200

    Definidas las funciones de actuaciones y rendimientos

--- turborreactor.py
+++ turborreactor.py
@@ -7,6 +7,7 @@ from isentropic_gas import IsentropicGas
 import isa
 air = RealGas(cp_option='naca', gamma_option='standard')
 gas = IsentropicGas(selected_cp_air_model='naca', selected_gamma_air_model='standard')
+import generador_gas
 
 
 # Cálculo del difusor
@@ -19,7 +20,8 @@ def difusor(mach, p0, T0):
 
 # Función rendimiento tobera
 def rend_tobera(x):
-    return -1/3 * ((x-8000)/11000)**2 + 1
+
+    return - 1/3 * ((x - 8000) / 11000) ** 2 + 1
 
 
 # Calculo de la tobera
@@ -27,11 +29,32 @@ def tobera(h, p5t, T5t, T2t):
     # Tobera
     p9 = isa.pres_isa(h)
     Ttob = (T5t + T2t) * 0.5
-    T9 = (((p9/p5t)**((air.gamma_air(Ttob) - 1)/air.gamma_air(Ttob)) - 1) * rend_tobera(h) + 1) * T5t
+    T9 = (((p9 / p5t) ** ((air.gamma_air(Ttob) - 1) / air.gamma_air(Ttob)) - 1) * rend_tobera(h) + 1) * T5t
 
     return T9, p9
 
 
-def empuje(G, c, v9, v0):
-    return (G + c)*v9 - G*v0
+def actuaciones(G0, c, v9, v0):
+    #Empuje
+    E = (G0 + c) * v9 - G0 * v0
+    #Empuje neto
+    Eneto = G0 * (v9 - v0)
+    # Impulso específico total (sin aproximar la presión de salida)
+    Ie = E / G0
+    # Consumo específico total (sin aproximar la presión de salida)
+    Ce = c / E
 
+    return E, Ie, Ce, Eneto,
+
+# Rendimiento motor, propulsor y motopropulsor:
+def rendimiento_TB(Eneto, G, c, v9, v0, ):
+    # Rendimiento motor:
+    eta_m = (Eneto * v0 + 0.5 * (G + c) * (v9 - v0) ** 2 - 0.5 * c * v0 ** 2) / (c * generador_gas.heating_value)
+
+    # Rendimiento propulsor:
+    eta_p = Eneto * v0 / (Eneto * v0 + 0.5 * (G + c) * (v9 - v0) ** 2 - 0.5 * c * v0 ** 2)
+
+    # Rendimiento motopropulsor:
+    eta_mp = eta_m * eta_p
+
+    return eta_m, eta_p, eta_mp
